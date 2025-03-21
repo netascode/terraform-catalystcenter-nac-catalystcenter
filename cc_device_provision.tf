097c1ed812b0b00f073b03fdcd9805d4ed557f55
kuba-mazurkiewicz <kmazurki@cisco.com>
2025-03-21 09:51:41 +0100

    allow port_assignment on FIAB

--- cc_device_provision.tf
+++ cc_device_provision.tf
@@ -199,7 +199,7 @@ locals {
 }
 
 resource "catalystcenter_fabric_port_assignment" "port_assignments" {
-  for_each = { for device in try(local.catalyst_center.inventory.devices, []) : device.name => device if device.state == "PROVISION" && device.device_role == "ACCESS" && contains(device.fabric_roles, "EDGE_NODE") && try(device.port_assignments, null) != null }
+  for_each = { for device in try(local.catalyst_center.inventory.devices, []) : device.name => device if device.state == "PROVISION" && contains(device.fabric_roles, "EDGE_NODE") && try(device.port_assignments, null) != null }
 
   fabric_id         = try(catalystcenter_fabric_site.fabric_site[each.value.fabric_site].id, null)
   network_device_id = local.device_ip_to_id[each.value.device_ip]
