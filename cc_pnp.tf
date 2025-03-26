2a59dd818f1181c8c5630e5e7dc92f4c93cedf4b
kuba-mazurkiewicz <kmazurki@cisco.com>
2025-03-26 21:05:53 +0100

    add config_parameters support in pnp_device_claim_site

--- cc_pnp.tf
+++ cc_pnp.tf
@@ -40,8 +40,8 @@ resource "catalystcenter_pnp_device_claim_site" "claim_device" {
   type              = try(each.value.type, local.defaults.catalyst_center.pnp.devices.type, null)
   image_id          = try(each.value.image_id, local.defaults.catalyst_center.pnp.devices.image_id, null)
   image_skip        = try(each.value.image_skip, local.defaults.catalyst_center.pnp.devices.image_skip, null)
-  config_id         = try(catalystcenter_template.regular_template[keys(each.value.onboarding_templates.regular)[0]].id, null)
-  config_parameters = try(each.value.onboarding_templates.regular[keys(each.value.onboarding_templates.regular)[0]].variables, local.defaults.catalyst_center.onboarding_templates.variables, null)
+  config_id         = try(catalystcenter_template.regular_template[each.value.onboarding_templates.regular[0].name].id, null)
+  config_parameters = try(each.value.onboarding_templates.regular[0].variables, local.defaults.catalyst_center.onboarding_templates.variables, null)
 
   depends_on = [catalystcenter_network_profile.switching_network_profile]
 }
