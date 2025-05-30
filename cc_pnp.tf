89c9e4fd68883af59ab9ce1bd6cf3a61179f4ae2
kuba-mazurkiewicz <kmazurki@cisco.com>
2025-05-30 09:41:55 +0200

    fixes in pnp

--- cc_pnp.tf
+++ cc_pnp.tf
@@ -9,7 +9,7 @@ resource "catalystcenter_pnp_device" "pnp_device" {
 resource "catalystcenter_pnp_device_claim_site" "claim_device" {
   for_each = { for device in try(local.catalyst_center.inventory.devices, []) : device.name => device if device.state == "PNP" }
 
-  device_id         = catalystcenter_pnp_device.pnp_device.id
+  device_id         = catalystcenter_pnp_device.pnp_device[each.key].id
   site_id           = local.site_id_list[each.value.site]
   type              = try(each.value.type, local.defaults.catalyst_center.pnp.devices.type, null)
   image_id          = try(each.value.image_id, local.defaults.catalyst_center.pnp.devices.image_id, null)
@@ -23,7 +23,7 @@ resource "catalystcenter_pnp_device_claim_site" "claim_device" {
 resource "catalystcenter_pnp_config_preview" "config_preview" {
   for_each = { for device in try(local.catalyst_center.inventory.devices, []) : device.name => device if device.state == "PNP" }
 
-  device_id = catalystcenter_pnp_device.pnp_device.id
+  device_id = catalystcenter_pnp_device.pnp_device[each.key].id
   site_id   = local.site_id_list[each.value.site]
   type      = try(each.value.type, local.defaults.catalyst_center.pnp.devices.type, null)
 
