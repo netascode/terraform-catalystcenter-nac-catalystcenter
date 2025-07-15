ca6c89c90163fb11c5631a1e63264a63db07b45e
kuba-mazurkiewicz <kmazurki@cisco.com>
2025-07-15 16:08:19 +0200

    add reprovision to wireless_device_provision

--- cc_device_provision.tf
+++ cc_device_provision.tf
@@ -71,6 +71,7 @@ resource "catalystcenter_wireless_device_provision" "wireless_controller" {
   site                 = try(each.value.site, null)
   network_device_id    = lookup(local.device_ip_to_id, each.value.device_ip, null)
   managed_ap_locations = try(each.value.managed_ap_locations, [each.value.site], null)
+  reprovision          = try(each.value.state, null) == "REPROVISION" ? true : false
 
   depends_on = [catalystcenter_building.building, catalystcenter_floor.floor, catalystcenter_area.area_0, catalystcenter_area.area_1, catalystcenter_area.area_2]
 }
