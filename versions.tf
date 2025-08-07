c1236d1f02df39c7db1b55fae0dd7b131eb3fd4c
kuba-mazurkiewicz <kmazurki@cisco.com>
2025-08-07 09:25:23 +0200

    bump up provider version

--- versions.tf
+++ versions.tf
@@ -4,7 +4,7 @@ terraform {
   required_providers {
     catalystcenter = {
       source  = "CiscoDevNet/catalystcenter"
-      version = ">= 0.2.16"
+      version = ">= 0.3.0"
     }
     utils = {
       source  = "netascode/utils"
