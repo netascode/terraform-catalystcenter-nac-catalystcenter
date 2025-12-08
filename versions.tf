1408517b13e944e3516ec1add1560d325666ed67
kuba-mazurkiewicz <kmazurki@cisco.com>
2025-12-08 22:09:58 +0100

    update provider version

--- versions.tf
+++ versions.tf
@@ -4,7 +4,7 @@ terraform {
   required_providers {
     catalystcenter = {
       source  = "CiscoDevNet/catalystcenter"
-      version = ">= 0.4.3"
+      version = ">= 0.4.4"
     }
     utils = {
       source  = "netascode/utils"
