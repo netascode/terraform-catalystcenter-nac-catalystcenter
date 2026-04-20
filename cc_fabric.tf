286de40a042ea9e7bf701481e471e4d29cc4d477
Rafal Slominski <rslominski@gmail.com>
2026-04-20 15:18:14 +0200

    use bare device name in fabric_rps

--- cc_fabric.tf
+++ cc_fabric.tf
@@ -883,7 +883,7 @@ locals {
               rp_device_location = try(rp.rp_location, null)
               network_device_ids = try(rp.rp_location, "") == "FABRIC" ? [
                 for device_name in try(rp.fabric_rps, []) :
-                try(local.device_name_to_id[device_name], null)
+                try(local.device_name_to_id[device_name], local.device_name_to_id[local.name_to_fqdn_mapping[device_name]], null)
               ] : null
             }
           ]
