f3c48c84b0c05740351ad098bbc10b0f2a9c2886
aberkimb <aberkimb@cisco.com>
2026-06-22 08:41:42 +0200

    Fix device credential objects failing to delete

--- versions.tf
+++ versions.tf
@@ -4,7 +4,7 @@ terraform {
   required_providers {
     catalystcenter = {
       source  = "CiscoDevNet/catalystcenter"
-      version = "~> 0.5.16"
+      version = "~> 0.5.18"
     }
     utils = {
       source  = "netascode/utils"
