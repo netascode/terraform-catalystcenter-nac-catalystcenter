1572b91903fcc62eb6eb7da8b95a35d4b5c66b7c
rafal-slominskii <rsominsk@cisco.com>
2026-08-25 16:03:03 +0200

    write only tf changes

--- cc_lan_automation.tf
+++ cc_lan_automation.tf
@@ -28,13 +28,14 @@ resource "catalystcenter_lan_automation" "lanauto_edge" {
     "ip_pool_name" : pool.name
     "ip_pool_role" : pool.role == "PRINCIPAL_IP_ADDRESS_POOL" ? "MAIN_POOL" : pool.role == "LINK_OVERLAPPING_IP_POOL" ? "PHYSICAL_LINK_POOL" : null
   }]
-  multicast_enabled        = try(each.value.multicast_enabled, local.defaults.catalyst_center.lan_automation.devices.multicast_enabled, null)
-  redistribute_isis_to_bgp = try(each.value.advertise_lan_automation_routes_into_bgp, local.defaults.catalyst_center.lan_automation.devices.advertise_lan_automation_routes_into_bgp, null)
-  isis_domain_password     = sensitive(try(each.value.isis_domain_password, local.defaults.catalyst_center.lan_automation.devices.isis_domain_password, null))
-  host_name_prefix         = try(each.value.host_name_prefix, local.defaults.catalyst_center.lan_automation.devices.host_name_prefix, null)
-  discovery_level          = try(each.value.discovery_level, local.defaults.catalyst_center.lan_automation.devices.discovery_level, null)
-  discovery_devices        = try(each.value.discovery_devices, local.defaults.catalyst_center.lan_automation.devices.discovery_devices, null)
-  discovery_timeout        = try(each.value.discovery_timeout, local.defaults.catalyst_center.lan_automation.devices.discovery_timeout, null)
+  multicast_enabled               = try(each.value.multicast_enabled, local.defaults.catalyst_center.lan_automation.devices.multicast_enabled, null)
+  redistribute_isis_to_bgp        = try(each.value.advertise_lan_automation_routes_into_bgp, local.defaults.catalyst_center.lan_automation.devices.advertise_lan_automation_routes_into_bgp, null)
+  isis_domain_password_wo         = sensitive(try(each.value.isis_domain_password, local.defaults.catalyst_center.lan_automation.devices.isis_domain_password, null))
+  isis_domain_password_wo_version = try(each.value.isis_domain_password_version, local.defaults.catalyst_center.lan_automation.devices.isis_domain_password_version, 1)
+  host_name_prefix                = try(each.value.host_name_prefix, local.defaults.catalyst_center.lan_automation.devices.host_name_prefix, null)
+  discovery_level                 = try(each.value.discovery_level, local.defaults.catalyst_center.lan_automation.devices.discovery_level, null)
+  discovery_devices               = try(each.value.discovery_devices, local.defaults.catalyst_center.lan_automation.devices.discovery_devices, null)
+  discovery_timeout               = try(each.value.discovery_timeout, local.defaults.catalyst_center.lan_automation.devices.discovery_timeout, null)
 
   depends_on = [catalystcenter_ip_pool_reservation.pool_reservation]
 }
--- cc_system_settings.tf
+++ cc_system_settings.tf
@@ -7,29 +7,33 @@ resource "catalystcenter_authentication_policy_server" "ise" {
 
   count = length(try(local.catalyst_center.system_settings.authentication_and_policy_servers.ise, [])) > 0 && (var.manage_global_settings || (!var.manage_global_settings && length(var.managed_sites) == 0)) ? 1 : 0
 
-  authentication_port      = try(local.catalyst_center.system_settings.authentication_and_policy_servers.ise.protocols.radius.authentication_port, local.defaults.catalyst_center.system_settings.authentication_and_policy_servers.ise.protocols.radius.authentication_port, null)
-  accounting_port          = try(local.catalyst_center.system_settings.authentication_and_policy_servers.ise.protocols.radius.accounting_port, local.defaults.catalyst_center.system_settings.authentication_and_policy_servers.ise.protocols.radius.accounting_port, null)
-  ip_address               = try(local.catalyst_center.system_settings.authentication_and_policy_servers.ise.ip_address, local.defaults.catalyst_center.system_settings.authentication_and_policy_servers.ise.ip_address, null)
-  pxgrid_enabled           = try(local.catalyst_center.system_settings.authentication_and_policy_servers.ise.pxgrid_enabled, local.defaults.catalyst_center.system_settings.authentication_and_policy_servers.ise.pxgrid_enabled, null)
-  use_dnac_cert_for_pxgrid = try(local.catalyst_center.system_settings.authentication_and_policy_servers.ise.use_catc_cert_for_pxgrid, local.defaults.catalyst_center.system_settings.authentication_and_policy_servers.ise.use_catc_cert_for_pxgrid, null)
-  is_ise_enabled           = true
-  port                     = local.ise_auth_policy_server_tacacs ? try(local.catalyst_center.system_settings.authentication_and_policy_servers.ise.protocols.tacacs.port, local.defaults.catalyst_center.system_settings.authentication_and_policy_servers.ise.protocols.tacacs.port, null) : null
-  protocol                 = local.ise_auth_policy_server_radius && local.ise_auth_policy_server_tacacs ? "RADIUS_TACACS" : local.ise_auth_policy_server_tacacs ? "TACACS" : local.ise_auth_policy_server_radius ? "RADIUS" : null
-  retries                  = try(local.catalyst_center.system_settings.authentication_and_policy_servers.ise.retries, local.defaults.catalyst_center.system_settings.authentication_and_policy_servers.ise.retries, null)
-  timeout_seconds          = try(local.catalyst_center.system_settings.authentication_and_policy_servers.ise.timeout, local.defaults.catalyst_center.system_settings.authentication_and_policy_servers.ise.timeout, null)
-  role                     = "primary"
-  shared_secret            = sensitive(try(local.catalyst_center.system_settings.authentication_and_policy_servers.ise.shared_secret, local.defaults.catalyst_center.system_settings.authentication_and_policy_servers.ise.shared_secret, null))
-  encryption_scheme        = try(local.catalyst_center.system_settings.authentication_and_policy_servers.ise.protocols.radius.enable_key_wrap, null) != null ? "KEYWRAP" : null
-  encryption_key           = sensitive(try(local.catalyst_center.system_settings.authentication_and_policy_servers.ise.protocols.radius.enable_key_wrap, null) != null ? try(local.catalyst_center.system_settings.authentication_and_policy_servers.ise.protocols.radius.enable_key_wrap.encryption_key, local.defaults.catalyst_center.system_settings.authentication_and_policy_servers.ise.protocols.radius.enable_key_wrap.encryption_key, null) : null)
-  message_key              = sensitive(try(local.catalyst_center.system_settings.authentication_and_policy_servers.ise.protocols.radius.enable_key_wrap, null) != null ? try(local.catalyst_center.system_settings.authentication_and_policy_servers.ise.protocols.radius.enable_key_wrap.message_key, local.defaults.catalyst_center.system_settings.authentication_and_policy_servers.ise.protocols.radius.enable_key_wrap.message_key, null) : null)
+  authentication_port       = try(local.catalyst_center.system_settings.authentication_and_policy_servers.ise.protocols.radius.authentication_port, local.defaults.catalyst_center.system_settings.authentication_and_policy_servers.ise.protocols.radius.authentication_port, null)
+  accounting_port           = try(local.catalyst_center.system_settings.authentication_and_policy_servers.ise.protocols.radius.accounting_port, local.defaults.catalyst_center.system_settings.authentication_and_policy_servers.ise.protocols.radius.accounting_port, null)
+  ip_address                = try(local.catalyst_center.system_settings.authentication_and_policy_servers.ise.ip_address, local.defaults.catalyst_center.system_settings.authentication_and_policy_servers.ise.ip_address, null)
+  pxgrid_enabled            = try(local.catalyst_center.system_settings.authentication_and_policy_servers.ise.pxgrid_enabled, local.defaults.catalyst_center.system_settings.authentication_and_policy_servers.ise.pxgrid_enabled, null)
+  use_dnac_cert_for_pxgrid  = try(local.catalyst_center.system_settings.authentication_and_policy_servers.ise.use_catc_cert_for_pxgrid, local.defaults.catalyst_center.system_settings.authentication_and_policy_servers.ise.use_catc_cert_for_pxgrid, null)
+  is_ise_enabled            = true
+  port                      = local.ise_auth_policy_server_tacacs ? try(local.catalyst_center.system_settings.authentication_and_policy_servers.ise.protocols.tacacs.port, local.defaults.catalyst_center.system_settings.authentication_and_policy_servers.ise.protocols.tacacs.port, null) : null
+  protocol                  = local.ise_auth_policy_server_radius && local.ise_auth_policy_server_tacacs ? "RADIUS_TACACS" : local.ise_auth_policy_server_tacacs ? "TACACS" : local.ise_auth_policy_server_radius ? "RADIUS" : null
+  retries                   = try(local.catalyst_center.system_settings.authentication_and_policy_servers.ise.retries, local.defaults.catalyst_center.system_settings.authentication_and_policy_servers.ise.retries, null)
+  timeout_seconds           = try(local.catalyst_center.system_settings.authentication_and_policy_servers.ise.timeout, local.defaults.catalyst_center.system_settings.authentication_and_policy_servers.ise.timeout, null)
+  role                      = "primary"
+  shared_secret_wo          = sensitive(try(local.catalyst_center.system_settings.authentication_and_policy_servers.ise.shared_secret, local.defaults.catalyst_center.system_settings.authentication_and_policy_servers.ise.shared_secret, null))
+  shared_secret_wo_version  = try(local.catalyst_center.system_settings.authentication_and_policy_servers.ise.shared_secret_version, local.defaults.catalyst_center.system_settings.authentication_and_policy_servers.ise.shared_secret_version, 1)
+  encryption_scheme         = try(local.catalyst_center.system_settings.authentication_and_policy_servers.ise.protocols.radius.enable_key_wrap, null) != null ? "KEYWRAP" : null
+  encryption_key_wo         = sensitive(try(local.catalyst_center.system_settings.authentication_and_policy_servers.ise.protocols.radius.enable_key_wrap, null) != null ? try(local.catalyst_center.system_settings.authentication_and_policy_servers.ise.protocols.radius.enable_key_wrap.encryption_key, local.defaults.catalyst_center.system_settings.authentication_and_policy_servers.ise.protocols.radius.enable_key_wrap.encryption_key, null) : null)
+  encryption_key_wo_version = try(local.catalyst_center.system_settings.authentication_and_policy_servers.ise.protocols.radius.enable_key_wrap.encryption_key_version, local.defaults.catalyst_center.system_settings.authentication_and_policy_servers.ise.protocols.radius.enable_key_wrap.encryption_key_version, 1)
+  message_key_wo            = sensitive(try(local.catalyst_center.system_settings.authentication_and_policy_servers.ise.protocols.radius.enable_key_wrap, null) != null ? try(local.catalyst_center.system_settings.authentication_and_policy_servers.ise.protocols.radius.enable_key_wrap.message_key, local.defaults.catalyst_center.system_settings.authentication_and_policy_servers.ise.protocols.radius.enable_key_wrap.message_key, null) : null)
+  message_key_wo_version    = try(local.catalyst_center.system_settings.authentication_and_policy_servers.ise.protocols.radius.enable_key_wrap.message_key_version, local.defaults.catalyst_center.system_settings.authentication_and_policy_servers.ise.protocols.radius.enable_key_wrap.message_key_version, 1)
 
   cisco_ise_dtos = [
     {
-      user_name       = try(local.catalyst_center.system_settings.authentication_and_policy_servers.ise.username, local.defaults.catalyst_center.system_settings.authentication_and_policy_servers.ise.username, null)
-      password        = sensitive(try(local.catalyst_center.system_settings.authentication_and_policy_servers.ise.password, local.defaults.catalyst_center.system_settings.authentication_and_policy_servers.ise.password, null))
-      fqdn            = try(local.catalyst_center.system_settings.authentication_and_policy_servers.ise.fqdn, local.defaults.catalyst_center.system_settings.authentication_and_policy_servers.ise.fqdn, null)
-      ip_address      = try(local.catalyst_center.system_settings.authentication_and_policy_servers.ise.ip_address, local.defaults.catalyst_center.system_settings.authentication_and_policy_servers.ise.ip_address, null)
-      subscriber_name = try(local.catalyst_center.system_settings.authentication_and_policy_servers.ise.ip_address, local.defaults.catalyst_center.system_settings.authentication_and_policy_servers.ise.ip_address, null)
+      user_name           = try(local.catalyst_center.system_settings.authentication_and_policy_servers.ise.username, local.defaults.catalyst_center.system_settings.authentication_and_policy_servers.ise.username, null)
+      password_wo         = sensitive(try(local.catalyst_center.system_settings.authentication_and_policy_servers.ise.password, local.defaults.catalyst_center.system_settings.authentication_and_policy_servers.ise.password, null))
+      password_wo_version = try(local.catalyst_center.system_settings.authentication_and_policy_servers.ise.password_version, local.defaults.catalyst_center.system_settings.authentication_and_policy_servers.ise.password_version, 1)
+      fqdn                = try(local.catalyst_center.system_settings.authentication_and_policy_servers.ise.fqdn, local.defaults.catalyst_center.system_settings.authentication_and_policy_servers.ise.fqdn, null)
+      ip_address          = try(local.catalyst_center.system_settings.authentication_and_policy_servers.ise.ip_address, local.defaults.catalyst_center.system_settings.authentication_and_policy_servers.ise.ip_address, null)
+      subscriber_name     = try(local.catalyst_center.system_settings.authentication_and_policy_servers.ise.ip_address, local.defaults.catalyst_center.system_settings.authentication_and_policy_servers.ise.ip_address, null)
     }
   ]
 }
@@ -45,18 +49,21 @@ resource "catalystcenter_authentication_policy_server" "aaa" {
 
   for_each = { for aaa in try(local.catalyst_center.system_settings.authentication_and_policy_servers.aaa, []) : aaa.ip_address => aaa if var.manage_global_settings || (!var.manage_global_settings && length(var.managed_sites) == 0) }
 
-  authentication_port = try(each.value.protocols.radius.authentication_port, local.defaults.catalyst_center.system_settings.authentication_and_policy_servers.aaa.protocols.radius.authentication_port, null)
-  accounting_port     = try(each.value.protocols.radius.accounting_port, local.defaults.catalyst_center.system_settings.authentication_and_policy_servers.aaa.protocols.radius.accounting_port, null)
-  ip_address          = each.key
-  port                = try(each.value.protocols.tacacs.port, local.defaults.catalyst_center.system_settings.authentication_and_policy_servers.aaa.protocols.tacacs.port, null)
-  protocol            = can(each.value.protocols.tacacs) && can(each.value.protocols.radius) ? "RADIUS_TACACS" : can(each.value.protocols.tacacs) ? "TACACS" : can(each.value.protocols.radius) ? "RADIUS" : null
-  retries             = try(each.value.retries, local.defaults.catalyst_center.system_settings.authentication_and_policy_servers.aaa.retries, null)
-  timeout_seconds     = try(each.value.timeout, local.defaults.catalyst_center.system_settings.authentication_and_policy_servers.aaa.timeout, null)
-  role                = "primary"
-  shared_secret       = sensitive(try(each.value.shared_secret, local.defaults.catalyst_center.system_settings.authentication_and_policy_servers.aaa.shared_secret, null))
-  encryption_scheme   = try(each.value.protocols.radius.enable_key_wrap, null) != null ? "KEYWRAP" : null
-  encryption_key      = sensitive(can(each.value.protocols.radius.enable_key_wrap) ? try(each.value.protocols.radius.enable_key_wrap.encryption_key, local.defaults.catalyst_center.system_settings.authentication_and_policy_servers.aaa.protocols.radius.enable_key_wrap.encryption_key, null) : null)
-  message_key         = sensitive(can(each.value.protocols.radius.enable_key_wrap) ? try(each.value.protocols.radius.enable_key_wrap.message_key, local.defaults.catalyst_center.system_settings.authentication_and_policy_servers.aaa.protocols.radius.enable_key_wrap.message_key, null) : null)
+  authentication_port       = try(each.value.protocols.radius.authentication_port, local.defaults.catalyst_center.system_settings.authentication_and_policy_servers.aaa.protocols.radius.authentication_port, null)
+  accounting_port           = try(each.value.protocols.radius.accounting_port, local.defaults.catalyst_center.system_settings.authentication_and_policy_servers.aaa.protocols.radius.accounting_port, null)
+  ip_address                = each.key
+  port                      = try(each.value.protocols.tacacs.port, local.defaults.catalyst_center.system_settings.authentication_and_policy_servers.aaa.protocols.tacacs.port, null)
+  protocol                  = can(each.value.protocols.tacacs) && can(each.value.protocols.radius) ? "RADIUS_TACACS" : can(each.value.protocols.tacacs) ? "TACACS" : can(each.value.protocols.radius) ? "RADIUS" : null
+  retries                   = try(each.value.retries, local.defaults.catalyst_center.system_settings.authentication_and_policy_servers.aaa.retries, null)
+  timeout_seconds           = try(each.value.timeout, local.defaults.catalyst_center.system_settings.authentication_and_policy_servers.aaa.timeout, null)
+  role                      = "primary"
+  shared_secret_wo          = sensitive(try(each.value.shared_secret, local.defaults.catalyst_center.system_settings.authentication_and_policy_servers.aaa.shared_secret, null))
+  shared_secret_wo_version  = try(each.value.shared_secret_version, local.defaults.catalyst_center.system_settings.authentication_and_policy_servers.aaa.shared_secret_version, 1)
+  encryption_scheme         = try(each.value.protocols.radius.enable_key_wrap, null) != null ? "KEYWRAP" : null
+  encryption_key_wo         = sensitive(can(each.value.protocols.radius.enable_key_wrap) ? try(each.value.protocols.radius.enable_key_wrap.encryption_key, local.defaults.catalyst_center.system_settings.authentication_and_policy_servers.aaa.protocols.radius.enable_key_wrap.encryption_key, null) : null)
+  encryption_key_wo_version = try(each.value.protocols.radius.enable_key_wrap.encryption_key_version, local.defaults.catalyst_center.system_settings.authentication_and_policy_servers.aaa.protocols.radius.enable_key_wrap.encryption_key_version, 1)
+  message_key_wo            = sensitive(can(each.value.protocols.radius.enable_key_wrap) ? try(each.value.protocols.radius.enable_key_wrap.message_key, local.defaults.catalyst_center.system_settings.authentication_and_policy_servers.aaa.protocols.radius.enable_key_wrap.message_key, null) : null)
+  message_key_wo_version    = try(each.value.protocols.radius.enable_key_wrap.message_key_version, local.defaults.catalyst_center.system_settings.authentication_and_policy_servers.aaa.protocols.radius.enable_key_wrap.message_key_version, 1)
 }
 
 resource "catalystcenter_external_authentication" "external_authentication" {
